f6569efc536f84a5ef826617d8b1a0c3c79d1475
Thomas McKee <thomasenmckee@gmail.com>
2025-04-08 20:22:59 -0500

    Add new website code pt.2

---
+++ build/js/main.js
@@ -0,0 +1,42 @@
+const texts = [
+    "Hello, I'm Thomas.",
+    "I'm a computer engineering student."
+];
+
+let index = 0;
+let charIndex = 0;
+let currentText = '';
+let isDeleting = false;
+const typingSpeed = 100;
+const deletingSpeed = 50;
+const pauseTime = 3000;
+
+const textElement = document.getElementById('text');
+
+function type() {
+    if (index >= texts.length) index = 0;
+    currentText = texts[index]
+
+    if (isDeleting) {
+        textElement.textContent = currentText.substring(0, charIndex--);
+        if (charIndex < 0) {
+            isDeleting = false;
+            index++;
+            setTimeout(type, 500);
+        } else {
+            setTimeout(type, deletingSpeed);
+        }
+    } else {
+        textElement.textContent = currentText.substring(0, charIndex++);
+        if (charIndex > currentText.length) {
+            isDeleting = true;
+            setTimeout(type, pauseTime);
+        } else {
+            setTimeout(type, typingSpeed);
+        }
+    }
+}
+
+window.onload = function() {
+    type();  // Call the typing function when the page loads
+};
